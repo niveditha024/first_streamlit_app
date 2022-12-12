0e51786171c35e675ab8a8d2dea23ae05517453a
Niveditha <120418024+niveditha024@users.noreply.github.com>
2022-12-12 15:14:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
